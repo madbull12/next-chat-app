ee36d0fae03451587247b8132f77f520f2777f71
madbull12 <huangandrian02@gmail.com>
2023-07-04 23:32:14 +0700

    loading suspense on requests page

--- src/app/(dashboard)/dashboard/requests/loading.tsx
+++ src/app/(dashboard)/dashboard/requests/loading.tsx
@@ -1,17 +1,27 @@
-import { Skeleton } from '@/components/ui/Skeleton'
-import React from 'react'
+import { Skeleton } from "@/components/ui/Skeleton";
+import React from "react";
 
 const Loading = () => {
   return (
-    <div className='ml-[25%]'>
-        <Skeleton className="w-[100px] h-[20px] " />
-        <Skeleton className="w-[100px] h-[20px] " />
-        <div className='flex items-center gap-x-2'>
-            <Skeleton className="w-[75px] h-[25px] " />
-        </div>
+    <div className="space-y-4 ml-[25%] p-4">
+      <Skeleton className="w-[100px] h-[30px] " />
 
+      {Array(5)
+        .fill(null)
+        .map((_, i) => (
+          <div key={i} >
+            <div className="flex items-start gap-x-2">
+              <Skeleton className="w-[40px] h-[40px] rounded-full" />
+              <div>
+                <Skeleton className="w-[100px] h-[30px] " />
+                <Skeleton className="w-[150px] h-[20px] " />
+                <Skeleton className="w-[80px] h-[40px] " />
+              </div>
+            </div>
+          </div>
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
